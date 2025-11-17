27809b03c9cc9b88af7639ac3cd9346cf9caf109
Darmatage <JasonWiserArt@gmail.com>
2025-11-16 23:37:25 -0500

    Updated placeholder environment art, player returns to map location

--- SleepWell/Assets/Scripts/Door_Interactive.cs
+++ SleepWell/Assets/Scripts/Door_Interactive.cs
@@ -9,8 +9,17 @@ public class Door_Interactive : MonoBehaviour{
         public GameObject msgPressE;
         public bool canPressE =false;
 
+//return to map location #1:
+  		private string thisLevel;
+		private Vector2 doorReturnPos;       // load with location for player when they return
+		public float offsetX = 0f;        // distance to left or right of the door for player to spawn
+		public float offsetY = 0f;        // distance above or below the door for player to spawn
+
        void Start(){
               msgPressE.SetActive(false);
+//return to map location #2:
+			  thisLevel = SceneManager.GetActiveScene().name;
+              doorReturnPos = new Vector2((transform.position.x + offsetX), (transform.position.y + offsetY)); 
         }
 
        void Update(){
@@ -34,6 +43,10 @@ public class Door_Interactive : MonoBehaviour{
         }
 
       public void EnterDoor(){
+//return to map location #3:
+			GameHandler_PlayerReturn.lastDoorPosition = doorReturnPos;
+			GameHandler_PlayerReturn.lastMap = thisLevel; 
+
             SceneManager.LoadScene (NextLevel);
       }
 
