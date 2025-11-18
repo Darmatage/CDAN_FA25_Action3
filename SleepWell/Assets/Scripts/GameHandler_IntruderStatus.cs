c220ca04ad8443ef73b6da6742998b4e4f8b76d4
Darmatage <JasonWiserArt@gmail.com>
2025-11-18 15:15:12 -0500

    Progressed intruder system

--- SleepWell/Assets/Scripts/GameHandler_IntruderStatus.cs
+++ SleepWell/Assets/Scripts/GameHandler_IntruderStatus.cs
@@ -6,31 +6,45 @@ using UnityEngine.SceneManagement;
 public class GameHandler_IntruderStatus : MonoBehaviour
 {
 
-	public int numIntruderOutside = 0;
-	public int numIntruderInside = 0;
+	public static int numIntruderOutside = 0;
+	public static int numIntruderInside = 0;
 
 //need a list of current intruders, their location
 
-	//public string[] peekRooms = {bathoom, kitchen, workoutroom, storagecloset, guestbedoom, garage};
-	//public string[] lookWindow = {bedroomSouth, bedroomWest, livingroomNorth, livingroomEast};
+	string[] peekRooms = {"bathoom", "kitchen", "workoutroom", "storagecloset", "guestbedoom", "garage"};
+	string[] lookWindow = {"Outside_BedroomSouth", "Outside_BedroomWest", "Outside_LivingroomNorth", "Outside_LivingroomEast"};
 
 //timers:
-	float intruderOutsideTimer = 5f;
-	public float intruderOutsideTime = 5f;
-	float intruderInsideTimer = 10f;
-	public float intruderInsideTime = 10f;
-	bool isOutside = false;
-	bool isInside = false;
+	float intruderOutsideTimer = 30f;
+	public float intruderOutsideTime = 30f;
+	float intruderInsideTimer = 40f;
+	public float intruderInsideTime = 40f;
+	public static bool isOutside = false;
+	public static bool isInside = false;
+
+	public bool playerAtTheWindowDoor = false;
+	string currentSceneName;
+	string intruderScene;
 
     void Start()
     {
-        
-    }
+		currentSceneName = SceneManager.GetActiveScene().name;
+		CheckPlayerLocation();
+	}
 
-//Timers
+//Timers for palyer doom OUTSIDE of pek-a-boo system at doors and wndows,
+//isOutside and isInside are the intruder status
     void FixedUpdate()
     {
-		if (isOutside)
+		//Just for testing:
+		if (Input.GetKeyDown("i"))
+		{
+			AddIntruder();
+			//Debug.Log("1. Hit letter [i]");
+		}
+
+		//timers for active intruder when the plyer is not at the right place: 
+		if (isOutside && !playerAtTheWindowDoor)
 		{
 			intruderOutsideTimer -= 0.01f;
 			if (intruderOutsideTimer <= 0)
@@ -41,27 +55,51 @@ public class GameHandler_IntruderStatus : MonoBehaviour
 				numIntruderOutside--;
 				numIntruderInside++;
 			}
-			else if (isInside)
-			{
-				intruderInsideTimer -= 0.01f;
-				if (intruderInsideTimer <= 0){
-					intruderInsideTimer = intruderInsideTime;
-					isOutside = false;
-					isInside = false;
-					//player loses
-				} 
-			}
 		}
-        
+		else if (isInside && !playerAtTheWindowDoor){
+			intruderInsideTimer -= 0.01f;
+			if (intruderInsideTimer <= 0){
+				intruderInsideTimer = intruderInsideTime;
+				isOutside = false;
+				isInside = false;
+				playerLoses();
+			} 
+		}        
     }
 
-
+//next, decide randomly which room, and use scenemanagement to id if we are in that room,
+//and if so, spawn an enemy
 //decide in a night that it is time to have an intruder, from day-night cycle sript
+
+
 
 	public void AddIntruder()
 	{
 		numIntruderOutside++;
 		isOutside = true;
+
+		//set start location of intuder:
+		int intruderLocationNum = Random.Range(0, lookWindow.Length);
+		intruderScene = lookWindow[intruderLocationNum];
+		//intruderScene = lookWindow[0];
+		Debug.Log("enemy is in:" + intruderScene);
+
+		CheckPlayerLocation();
+	}
+
+	void CheckPlayerLocation()
+	{
+		if (currentSceneName != null){
+			if (currentSceneName == intruderScene)
+			{
+				playerAtTheWindowDoor = true;
+				GameObject.FindWithTag("IntruderPlace").GetComponent<Intruder_SceneSystem>().StartIntruder();
+			}
+			else
+			{
+				playerAtTheWindowDoor = false;
+			}
+		}
 	}
 
 	public void CatchIntruder(string location)
@@ -78,8 +116,29 @@ public class GameHandler_IntruderStatus : MonoBehaviour
 		}
 	}
 
-//next, decide randomly which room, and use scenemanagement to id if we are in that room,
-//and if so, spawn an enemy
+	public void IntruderFinishedStage()
+	{
+		if (isOutside)
+		{
+			numIntruderOutside--;
+			isOutside = false;
+			numIntruderInside++;
+			isInside = true;
+			Debug.Log("Intruder got into the house!");
+		} 
+		else
+		{
+			numIntruderInside--;
+			isInside = false;
+			playerLoses();
+		}
+	}
 
+
+
+	void playerLoses()
+	{
+		Debug.Log("YOU LOSE");
+	}
 
 }
--- SleepWell/Assets/Scripts/Intruder_SceneSystem.cs
+++ SleepWell/Assets/Scripts/Intruder_SceneSystem.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class Intruder_System : MonoBehaviour
+public class Intruder_SceneSystem : MonoBehaviour
 {
 
 	public Transform[] path1;
 	public Transform[] path2;
 	public Transform[] path3;
 	public Transform[] path4;
+	private Transform[] thePath;
 
 	public GameObject intruder;
+	private GameObject theIntruder;
 	GameObject currentIntruder;
 
-	private float theTimer = 5;
+	private float timeBetweenIntrude = 5;
 	private int pathCount = 4;
 	public bool isIntruder = false;
+	float intrudeScale = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,7 +26,7 @@ public class Intruder_System : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    /*
     void FixedUpdate()
     {
         if (isIntruder)
@@ -36,7 +41,48 @@ public class Intruder_System : MonoBehaviour
 			}
 		}
     }
+	*/
 
+//spawn an intrudr, activated by the GameHandler_IntruderStatus:
+	public void StartIntruder()
+	{
+		int startNum = Random.Range(1,5);
+		if (startNum == 1){thePath = path1;} 
+		else if (startNum == 2){thePath = path2;}
+		else if (startNum == 3){thePath = path3;}		
+		else {thePath = path4;}
 
+		theIntruder = Instantiate(intruder, thePath[0].position, Quaternion.identity);
+		StartCoroutine(MoveIntruder(theIntruder));
+		Debug.Log("intruder added to scene, located at " + thePath[0].position);
+	}
+
+	IEnumerator MoveIntruder(GameObject thisIntruder)
+	{
+		Transform intT = thisIntruder.transform;
+		SpriteRenderer intruderSprite = thisIntruder.GetComponentInChildren<SpriteRenderer>();
+
+		yield return new WaitForSeconds(timeBetweenIntrude);
+
+		intT.position = thePath[1].position;
+		intT.localScale = new Vector3(intT.localScale.x  * intrudeScale, intT.localScale.y * intrudeScale, intT.localScale.z);
+		intruderSprite.sortingOrder = 25;
+		yield return new WaitForSeconds(timeBetweenIntrude);
+
+		intT.position = thePath[2].position;
+		intT.localScale = new Vector3(intT.localScale.x  * intrudeScale, intT.localScale.y * intrudeScale, intT.localScale.z);
+		intruderSprite.sortingOrder = 35;
+		yield return new WaitForSeconds(timeBetweenIntrude);
+
+		intT.transform.position = thePath[3].position;
+		intT.localScale = new Vector3(intT.localScale.x  * intrudeScale, intT.localScale.y * intrudeScale, intT.localScale.z);
+		intruderSprite.sortingOrder = 45;
+		yield return new WaitForSeconds(timeBetweenIntrude);
+
+		//jump scare!
+		intT.localScale = new Vector3(intT.localScale.x  * intrudeScale, intT.localScale.y * intrudeScale, intT.localScale.z);
+		GameObject.FindWithTag("GameHandler").GetComponent<GameHandler_IntruderStatus>().IntruderFinishedStage();
+		Destroy (thisIntruder);
+	}
 
 }
