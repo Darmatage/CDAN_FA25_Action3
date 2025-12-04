ad5e3e79fe1407601e6129d88f7fba1da399887b
amgonzalez-boop <amgonzalez@massart.edu>
2025-12-04 18:34:06 -0500

    SleepBar Stuff

---
+++ SleepWell/Assets/Scripts/SleepChargeTimer.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleTimer : MonoBehaviour {
+       public float timerMax = 10f;       //set the number of seconds here
+       private float theTimer = 0f;
+       public bool doTheThing = false;
+
+       public Image timerCircleDisplay;
+
+      void Start(){
+           timerCircleDisplay.gameObject.SetActive(false);
+           theTimer = timerMax;
+      }
+
+      void Update(){
+            //test functionality. Normally set=true by external script.
+            if (Input.GetKeyDown("8")){
+                  doTheThing = true;
+            }
+      }
+
+       void FixedUpdate(){
+            if (doTheThing == true){
+                  theTimer -= 0.001f;
+                  Debug.Log("time: " + theTimer);
+                  timerCircleDisplay.gameObject.SetActive(true);
+                  timerCircleDisplay.fillAmount = theTimer / timerMax;
+
+                  if (theTimer <= 0){
+                        theTimer = timerMax;
+                        Debug.Log("I do the thing!");       //can be replaced with the desired commands
+                        doTheThing = false;
+                    }
+              }
+       }
+
+       //public function to be accessed by other scripts to activate the timer.
+       public void TimeToDoTheThing(){
+              doTheThing = true;
+              //other commands when turnign on timer can go here.
+       }
+}
